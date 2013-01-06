639e203499cf868ff282bd30a851e71f928321f4
Chaosbohne <muhai@gmx.de>
2013-01-06 19:28:13 +0100

    init

--- static/scripts/markerProvider.js
+++ static/scripts/markerProvider.js
@@ -116,7 +116,7 @@ MarkerProvider = function(myMarkerData, map) {
   
     console.log(this.marker.station);
     var table = 
-      "<tr>"
+      "<tr id=" + this.marker.station._id + ">"
       + "<td class=\"first_column_detail\">"
       + "<img src=\"/images/yellowMarker.png\" alt=\"Petrol\" height=\"34\" width=\"21\">"
       + "</td>" 
@@ -215,7 +215,10 @@ MarkerProvider = function(myMarkerData, map) {
           +"</td><td class=\"second_column_route\"><label class=\"start_route\">"
           +start_address
           +"</label>"           
-          +"</td></tr></table>"
+          +"</td><td class=\"third_column_detail\">"
+          +"<button class=\"back_button\">"
+          +"<span class=\"back_span\">" + "</span>"
+          +"</button></td></tr></table>"
           +"<table id=\"route_steps\">"
           +steps
           +"</table>"
@@ -232,89 +235,6 @@ MarkerProvider = function(myMarkerData, map) {
          callback(null);
        }
     });
-  
-/*
-  var directionsService = new google.maps.DirectionsService();
-  this.directionsDisplay = new google.maps.DirectionsRenderer({
-    draggable: false,
-    suppressInfoWindow: true,
-    suppressMarkers: true});
-    
-  this.directionsDisplay.setMap(this.marker.getMap());
-
-    var request = {
-      origin:start,
-      destination: this.marker.position,
-      travelMode: google.maps.TravelMode.DRIVING
-    };
-    
-    _this = this;
-    
-    directionsService.route(request, function(result, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        console.log(result);
-        _this.directionsDisplay.setDirections(result);
-        //_this.directionsDisplay.setPanel(document.getElementById("directionsPanel"));
-        
-        var p = _this.directionsDisplay.getPanel();
-        //var a = p.getElementById('adp-placemark');
-
-        
-        var start_index = result.routes[0].legs[0].start_address.indexOf(', Deutschland');
-        var end_index = result.routes[0].legs[0].end_address.indexOf(', Deutschland');
-        var start_address;
-        var end_address;
-        if(start_index != -1) {
-          start_address = result.routes[0].legs[0].start_address.substring(0, start_index);
-        }else {
-          start_address = result.routes[0].legs[0].start_address;
-        }        
-        
-        if(end_index != -1) {
-          end_address = result.routes[0].legs[0].end_address.substring(0, end_index);
-        } else {
-          end_address = result.routes[0].legs[0].end_address;
-        }
-        
-        
-        var steps = '';
-        
-        for(var i = 0; i < result.routes[0].legs[0].steps.length; i++) {
-          steps += "<tr><td class=\"first_column_route\"><label>"+ i + "." +"</label></td>"
-          +"<td class=\"second_column_route\"><label>"
-          +result.routes[0].legs[0].steps[i].instructions
-          +"</label></td>"
-          +"<td><label>"
-          +result.routes[0].legs[0].steps[i].distance.text
-          +"</label></td></tr>";
-        }
-        
-        
-        var source = 
-            "<div>"
-          +"<table id=\"route_start\"><tr><td class=\"first_column_route\">" 
-          +"<img src=\"/images/positionA.png\" alt=\"Petrol\" height=\"34\" width=\"21\">"
-          +"</td><td class=\"second_column_route\"><label class=\"start_route\">"
-          +start_address
-          +"</label>"           
-          +"</td></tr></table>"
-          +"<table id=\"route_steps\">"
-          +steps
-          +"</table>"
-          +"<table id=\"route_end\"><tr><td class=\"first_column_route\">" 
-          +"<img src=\"/images/yellowMarker.png\" alt=\"Petrol\" height=\"34\" width=\"21\">"
-          +"</td><td class=\"second_column_route\"><label class=\"end_route\">"
-          +end_address
-          +"</label>"           
-          +"</td></tr></table>"
-          +"<label class=\"maps_copyright\">"+result.routes[0].copyrights+"</label>"
-          +"</div>";
-
-        
-      }
-      
-    });  
-*/
   }
   
   MarkerProvider.prototype.removeDirectionsResult = function() {
--- static/scripts/markerProviderList.js
+++ static/scripts/markerProviderList.js
@@ -121,6 +121,67 @@ MarkerProviderList = function(myMarkerData, map) {
       this.clickDetailHander();
       this.clickDeleteHandler();
       this.clickRouteHandler();
+      this.initMarkerHandler();
+    }
+    
+    MarkerProviderList.prototype.initMarkerHandler = function() {
+      var MarkerImage = new MarkerImageProvider();
+      var pinImageYellow = MarkerImage.getPinImageYellow();  
+      
+      var ___this = this;
+      for(var i = 0; i < listOfMarkers.length; i++) {
+        google.maps.event.clearListeners(listOfMarkers[i].getMarker(), 'mouseover');
+        google.maps.event.clearListeners(listOfMarkers[i].getMarker(), 'mouseout');
+        google.maps.event.clearListeners(listOfMarkers[i].getMarker(), 'click');
+        
+        google.maps.event.addListener(listOfMarkers[i].getMarker(), 'mouseover', function() {
+        
+          var markerID = this.station._id;
+          _this = this;
+          $('#petrol_container tr').each( function() {  
+            var id = $(this).attr('id');
+            if(id == markerID) {
+              _this.lastIconMarker = _this.getIcon();
+              _this.setIcon(pinImageYellow);
+              _this.lastIconData = $('#'+id +' img').attr('src');
+              $('#'+id +' img').attr({'src':'/images/yellowMarker.png' });
+            }
+          });
+        });
+        
+        google.maps.event.addListener(listOfMarkers[i].getMarker(), 'mouseout', function() {
+          
+          var markerID = this.station._id;
+          _this = this;
+          $('#petrol_container tr').each( function() {
+            var id = $(this).attr('id');
+            if(id == markerID) {
+              _this.setIcon(_this.lastIconMarker);
+              $('#'+id +' img').attr({'src':_this.lastIconData});
+            }
+          });          
+        });    
+
+        google.maps.event.addListener(listOfMarkers[i].getMarker(), 'click', function() {
+          google.maps.event.clearListeners(this, 'mouseout');
+          var MarkerImage = new MarkerImageProvider();
+          var pinImageYellow = MarkerImage.getPinImageYellow();            
+          var markerID = this.station._id;
+          for(var j = 0; j < listOfMarkers.length; j++) {
+            if(listOfMarkers[j].getID() == markerID) {
+              $('#petrol_detail tr').remove();
+              var table = listOfMarkers[j].getHtmlDetail();
+              this.setIcon(pinImageYellow);
+              $('#petrol_detail').append(table);
+              $('#petrol_overview').css({'display':'none'});
+              $('#directionsPanel').css({'display':'none'});
+              $('#petrol_detail').css({'display':'table'});
+              ___this.clickRouteHandler();
+              ___this.clickBackHandler();                
+            }
+          }          
+        });        
+      }
     }
     
     MarkerProviderList.prototype.initButtonHandler = function() {
@@ -214,7 +275,7 @@ MarkerProviderList = function(myMarkerData, map) {
   
     MarkerProviderList.prototype.clickRouteHandler = function() {
       var _this = this;
-      $('#petrol_overview .calc_button').click(function() {
+      $('#petrol_container .calc_button').click(function() {
         var id = $(this).closest('tr').attr('id');
          for(var i = 0; i < listOfMarkers.length; i++) {
            if(listOfMarkers[i].getID() == id) {                   
@@ -223,8 +284,9 @@ MarkerProviderList = function(myMarkerData, map) {
               $('#petrol_overview').css({'display':'none'});  
               $('#petrol_route').append(result);
               $('#directionsPanel').css({'display':'block'});  
+              _this.clickBackHandler();
              });
-
+            
            }
          } 
       });    
